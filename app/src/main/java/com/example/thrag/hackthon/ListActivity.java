aa84c3fb9af800267a0fb092a40e26e88c97a9a6
odekeyser <dksrolivier@gmail.com>
2015-03-25 20:06:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/thrag/hackthon/ListActivity.java
@@ -0,0 +1,7 @@
+package com.example.thrag.hackthon;
+
+/**
+ * Created by Thrag on 25/03/15.
+ */
+public class ListActivity {
+}
---
+++ app/src/main/java/com/example/thrag/hackthon/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.thrag.hackthon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    Button list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (Button)findViewById(R.id.buttonList);
+        list.setOnClickListener(listener);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener()
+    {
+        public void onClick(View view)
+        {
+            switch (view.getId())
+            {
+                case R.id.buttonList:
+
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
