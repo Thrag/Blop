77b8dd12fff75fe3ffe65796ec6ee6fa9b677557
Delbauve Sylvain <theamenai@hotmail.fr>
2015-03-26 10:31:02 +0100

    Kawabounga

--- app/src/main/java/com/example/thrag/hackthon/MainActivity.java
+++ app/src/main/java/com/example/thrag/hackthon/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 
     Button list;
     // Wesh ma gueule !
-
+    // Kawabounga MA GEULE :D
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
