dc09bd12a652383b6b4d327631de6c7d9fc761e6
Martin <martin.c@hotmail.com>
2015-03-26 10:25:42 +0100

    test commit martin

--- app/src/main/java/com/example/thrag/hackthon/MainActivity.java
+++ app/src/main/java/com/example/thrag/hackthon/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     Button list;
+    // Wesh ma gueule !
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
